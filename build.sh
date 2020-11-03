e47ab61d449ee01e4366798de595d98f342c9233
Nathan Perkins <nperkins487@gmail.com>
2020-11-02 22:59:42 -0800

    rename reddit_job to top_post_set

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -f docker/reddit_job/Dockerfile -t localhost:5000/reddit-job .
+docker build -f docker/top_post_set/Dockerfile -t localhost:5000/reddit-job .
 docker push localhost:5000/reddit-job
