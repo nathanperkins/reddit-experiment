127a7e8d0b9607a1a6c650deaf97abb77dc724c1
Nathan Perkins <nperkins487@gmail.com>
2020-11-02 23:01:02 -0800

    remove extraneous yaml deletes

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-kubectl delete -f kubernetes/secrets/reddit-auth.yaml || true
-kubectl delete -f kubernetes/cronjobs/top-post.yaml || true
-kubectl delete -f kubernetes/deployments/redis.yaml || true
-kubectl delete -f kubernetes/services/redis.yaml || true
-
 kubectl apply -f kubernetes/secrets/reddit-auth.yaml
 kubectl apply -f kubernetes/cronjobs/top-post.yaml
 kubectl apply -f kubernetes/deployments/redis.yaml
