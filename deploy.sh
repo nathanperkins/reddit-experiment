80b3a6f7dce4f7edff6b9782e057672b4fdf426c
Nathan Perkins <nperkins487@gmail.com>
2020-11-02 23:49:40 -0800

    rename kubernetes folders as singular

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-kubectl apply -f kubernetes/secrets/reddit-auth.yaml
-kubectl apply -f kubernetes/deployments/redis.yaml
-kubectl apply -f kubernetes/services/redis.yaml
-kubectl apply -f kubernetes/cronjobs/top-post-set.yaml
-kubectl apply -f kubernetes/deployments/top-post-get.yaml
-kubectl apply -f kubernetes/services/top-post-get.yaml
+kubectl apply -f kubernetes/secret/reddit-auth.yaml
+kubectl apply -f kubernetes/deployment/redis.yaml
+kubectl apply -f kubernetes/service/redis.yaml
+kubectl apply -f kubernetes/cronjob/top-post-set.yaml
+kubectl apply -f kubernetes/deployment/top-post-get.yaml
+kubectl apply -f kubernetes/service/top-post-get.yaml
